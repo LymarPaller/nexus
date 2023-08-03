192fe7f57b804d02b54b2c0f7bcc620aeffa8294
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-08-03 16:36:45 +0800

    feat: login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,41 @@
+import { Link } from 'react-router-dom';
+import '../styles/main.scss';
+import '../styles/normalize.scss';
+import Logo from '../assets/nexus-logo-blue.svg';
+import '../styles/LoginPage.scss';
+
 function LoginPage () {
     return (
         <>
-        <h1>Login Page</h1>
+        <div>
+
+            <div className='logo-container'>
+                <img src={Logo} alt="Nexus-logo" />
+            </div>
+
+            <div className='card-container-login'>
+
+                <div className='image-main-container'>
+                    <div className='image-container-login-front'>
+                    </div>
+                    <div className='image-container-login-back'>
+                    </div>
+                </div>
+
+                <div className='login-container'>
+                    <h1>NeXus</h1>
+                    <form action="" className='form-login'>
+                        <input type="text" placeholder='Username' required/>
+                        <input type="password"  placeholder='Password' required/>
+                        <div className='button-container'>
+                            <button type='submit'>Login</button>
+                            <Link className='create-new-account'>Create New Account</Link>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
         </>
     );
 };
