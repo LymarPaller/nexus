e36621d81fe6dcbcb1c1cd7780dc5493f3708779
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-05 02:08:46 +0800

    style: add placeholder in center div post newsfeed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,40 @@
 import "../styles/Feed.scss";
+import Profile from '../assets/wanda.jpg';
+
 
 function Feed () {
     return (
         <>
         <div className="feed-container">
             <div className="profile-feed-details">
-                <div className="profile-pic"> </div>
+            <div className="profile-info">
+            <div className="profile-image">
+              <img src={Profile} alt="Profile" />
+            </div>
+            <div className="name-time">
+              <h3>Wanda Zurbano</h3>
+              <p>1 minute ago</p>
+            </div>
+          </div>
+          <div className="delete-icon">
+            {/* Add delete icon modal upper right side */}
+          </div>
+          <div className="captions">
+            <p>Sample captions here</p>
+          </div>
+          <div className="post-image">
+          <img src={Profile} alt="Profile" />
+          </div>
+
+          <div className="likes-comments">
+            <div className="likes">
+              <p>100 Likes</p>
+            </div>
+            <div className="comments">
+              <p>50 Comments</p>
+            </div>
+          </div>
+
             </div>
 
         </div>
