66a06a6629bf6e88ee198123f9ec975cb29b98d7
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-05 00:49:18 +0800

    style: add placeholder in aside left homepage

--- src/components/AsideLeft.jsx
+++ src/components/AsideLeft.jsx
@@ -1,13 +1,43 @@
 import "../styles/AsideLeft.scss"
+import Profile from '../assets/wanda.jpg'
 
-function AsideLeft () {
+function AsideLeft() {
     return (
         <>
-        <div className="aside-left-container">
-            <h1>left</h1>
-        </div>
+            <div className="aside-left-container">
+                <div className="profile-info">
+                    <div className="profile">
+                        <div className="profile-image">
+                            <img src={Profile} alt="Profile" />
+                        </div>
+                        <h3>Wanda Zurbano</h3>
+                    </div>
+                    <div className="friends-section">
+                        <div className="profile-image">
+                            <img src={Profile} alt="Friend Profile" />
+                        </div>
+                        <h3>Friends</h3>
+                        {/* Render list of friends */}
+                    </div>
+                    <div className="groups-section">
+                        <div className="profile-image">
+                            <img src={Profile} alt="Group Profile" />
+                        </div>
+                        <h3>Groups</h3>
+                        {/* Render list of groups */}
+                    </div>
+                    <div className="memories-section">
+                        <div className="profile-image">
+                            <img src={Profile} alt="Memory Profile" />
+                        </div>
+                        <h3>Memories</h3>
+                        {/* Render list of memories */}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
 export default AsideLeft
+
