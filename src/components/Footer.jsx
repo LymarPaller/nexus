c07a8c69ba6884377a1253d1c18f3a4fb6ef0fbb
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-05 22:05:24 +0800

    refactor: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import '../styles/Footer.scss';
 import EmailLink from '../app/linkEmail';
 
 
+
 function Footer() {
+    const klaneZurbanoFacebook = 'https://www.facebook.com/klanezurbano';
+    const kodegoFacebook = 'https://www.facebook.com/KodegoPH'
+    const facebookLink = 'https://www.facebook.com/cantleavethisempty';
+    const instagramLink = 'https://www.instagram.com/your-instagram-link';
+    const lymarPallerEmail = 'lymarpaller@gmail.com';
+
     return (
         <>
             <footer className="footer">
@@ -14,14 +21,16 @@ function Footer() {
                         <div className="footer-subrow">
                             <div className="footer-title">Capstone Project by:</div>
                             <div>Mariya Villamor</div>
-                            <EmailLink recipient="mariyavillamor@gmail.com"/>
+                            <EmailLink recipient="mariyavillamor@gmail.com" />
                             <div className='name-spacing'>Lymar Paul Paller</div>
-                            <EmailLink recipient="lymarpaller@gmail.com"/>
+                            <EmailLink recipient="lymarpaller@gmail.com" />
                         </div>
                     </div>
                     <div className="footer-column">
                         <div className="footer-subrow">
-                            <div className="footer-title">Kodego</div>
+                            <div className="footer-title">
+                                <a href={kodegoFacebook} target="_blank" rel="noopener noreferrer">Kodego</a>
+                            </div>
                             <div>Batch WD73</div>
                             <div>Trailblazers</div>
                         </div>
@@ -29,16 +38,33 @@ function Footer() {
                     <div className="footer-column">
                         <div className="footer-subrow">
                             <div className="footer-title">Instructor</div>
-                            <div>Klane Zurbbano</div>
+                            <div>
+                                <a className="klane" href={klaneZurbanoFacebook} target="_blank" rel="noopener noreferrer">
+                                    Klane Zurbano
+                                </a>
+                            </div>
+
                             <div>Instructor Batch WD73</div>
                         </div>
                     </div>
                     <div className="footer-column">
                         <div className="footer-subrow">
                             <div className="footer-title">Contact Us</div>
-                            <div>Facebook</div>
-                            <div>Instagram</div>
-                            <div>Email</div>
+                            <div>
+                                <a href={facebookLink} target="_blank" rel="noopener noreferrer">
+                                    Facebook
+                                </a>
+                            </div>
+                            <div>
+                                <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                                    Instagram
+                                </a>
+                            </div>
+                            <div>
+                                <a href={`mailto:${lymarPallerEmail}`}>
+                                    Email
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
