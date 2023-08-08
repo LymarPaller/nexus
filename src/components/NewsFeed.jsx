1ac18a8e16f2c78d4a524bdd24f0ec50e75c37cb
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-08-09 00:24:31 +0800

    feat: added manual modal for posting in feed

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -6,6 +6,7 @@ import Profile from '../assets/wanda.jpg';
 function NewsFeed () {
     const [post, setPost] = useState('');
     const [feedItems, setFeedItems] = useState([]);
+    const [isPostModalOpen, setIsPostModalOpen] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -23,23 +24,43 @@ function NewsFeed () {
         setPost('');
     };
 
+    const handleClickToPost = () => {
+        const postModal = document.querySelector('.create-post');
+        if (postModal) {
+            postModal.classList.add('new-create-post');
+        }
+    }
+
+    const handleCancelPost = () => {
+        const postModal = document.querySelector('.create-post');
+        if (postModal) {
+            postModal.classList.remove('new-create-post');
+        }
+    }
+
     return (
         <>
         <div className="news-feed-container">
             <div className="create-post">
                 <div className="profile-pic"> 
-                <img src={Profile} alt="Profile" />
+                    <img src={Profile} alt="Profile" />
                 </div>
-                <form onSubmit={handleSubmit}>
-                        <input
+                <div className='upload-photo'>
+
+                </div>
+                <form onSubmit={handleSubmit} className='form-container'>
+                        <textarea
                             type="text"
                             className="input-feed"
                             placeholder="What's on your mind?"
                             value={post}
                             onChange={(e) => setPost(e.target.value)}
+                            onClick={handleClickToPost}
                         />
                         <button type="submit">Post</button>
-                    </form>
+                </form>
+                
+                <button type="cancel" className="cancel-button"onClick={handleCancelPost}>Cancel</button>
             </div>
             <Feed/>
         </div>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,8 +4,11 @@ import ProfileDetail from "../components/ProfileDetail";
 import Feed from "../components/Feed";
 import "../styles/ProfilePage.scss";
 import "../styles/ProfileFeed.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function ProfilePage () {
+
     return (
         <>
         <div className="profile-page-main-container">
