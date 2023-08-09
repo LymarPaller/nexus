9690ace795990b4c3ad3032ee0b238f2e54aca25
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-09 12:38:11 +0800

    refactor: added alt text for new tab links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,76 +1,78 @@
 import '../styles/Footer.scss';
 import EmailLink from '../app/linkEmail';
 
+const socialLinks = [
+    { text: 'Facebook', link: 'https://www.facebook.com/cantleavethisempty' },
+    { text: 'Instagram', link: 'https://www.instagram.com' },
+];
 
-
-function Footer() {
-    const klaneZurbanoFacebook = 'klanezurbano@gmail.com';
-    const kodegoFacebook = 'https://www.facebook.com/KodegoPH'
-    const facebookLink = 'https://www.facebook.com/cantleavethisempty';
-    const instagramLink = 'https://www.instagram.com/your-instagram-link';
+const Footer = () => {
+    const klaneZurbanoEmail = 'klanezurbano@gmail.com';
+    const kodegoFacebook = 'https://www.facebook.com/KodegoPH';
     const lymarPallerEmail = 'lymarpaller@gmail.com';
 
+    const renderSocialLinks = () => {
+        return (
+            <div>
+                {socialLinks.map((linkObj, index) => (
+                    <div key={index}>
+                        <a href={linkObj.link} target="_blank" rel="noopener noreferrer">
+                            {linkObj.text}
+                        </a>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
     return (
-        <>
-            <footer className="footer">
-                <div className="footer-row-1">
-                    Nexus copyright 2023
+        <footer className="footer">
+            <div className="footer-row-1">
+                Nexus copyright 2023
+            </div>
+            <div className="footer-row-2">
+                <div className="footer-column">
+                    <div className="footer-subrow">
+                        <div className="footer-title">Capstone Project by:</div>
+                        <div>Mariya Villamor</div>
+                        <EmailLink recipient="mariyavillamor@gmail.com" />
+                        <div className='name-spacing'>Lymar Paul Paller</div>
+                        <EmailLink recipient="lymarpaller@gmail.com" />
+                    </div>
                 </div>
-                <div className="footer-row-2">
-                    <div className="footer-column">
-                        <div className="footer-subrow">
-                            <div className="footer-title">Capstone Project by:</div>
-                            <div>Mariya Villamor</div>
-                            <EmailLink recipient="mariyavillamor@gmail.com" />
-                            <div className='name-spacing'>Lymar Paul Paller</div>
-                            <EmailLink recipient="lymarpaller@gmail.com" />
+                <div className="footer-column">
+                    <div className="footer-subrow">
+                        <div className="footer-title">
+                            <a href={kodegoFacebook} target="_blank" rel="noopener noreferrer">Kodego</a>
                         </div>
+                        <div>Batch WD73</div>
+                        <div>Trailblazers</div>
                     </div>
-                    <div className="footer-column">
-                        <div className="footer-subrow">
-                            <div className="footer-title">
-                                <a href={kodegoFacebook} target="_blank" rel="noopener noreferrer">Kodego</a>
-                            </div>
-                            <div>Batch WD73</div>
-                            <div>Trailblazers</div>
+                </div>
+                <div className="footer-column">
+                    <div className="footer-subrow">
+                        <div className="footer-title">Instructor</div>
+                        <div>
+                            <a className="klane" href={klaneZurbanoEmail} target="_blank" rel="noopener noreferrer">
+                                Klane Zurbano
+                            </a>
                         </div>
+                        <div>Instructor Batch WD73</div>
                     </div>
-                    <div className="footer-column">
-                        <div className="footer-subrow">
-                            <div className="footer-title">Instructor</div>
-                            <div>
-                                <a className="klane" href={klaneZurbanoFacebook} target="_blank" rel="noopener noreferrer">
-                                    Klane Zurbano
-                                </a>
-                            </div>
-
-                            <div>Instructor Batch WD73</div>
-                        </div>
-                    </div>
-                    <div className="footer-column">
-                        <div className="footer-subrow">
-                            <div className="footer-title">Contact Us</div>
-                            <div>
-                                <a href={facebookLink} target="_blank" rel="noopener noreferrer">
-                                    Facebook
-                                </a>
-                            </div>
-                            <div>
-                                <a href={instagramLink} target="_blank" rel="noopener noreferrer">
-                                    Instagram
-                                </a>
-                            </div>
-                            <div>
-                                <a href={`mailto:${lymarPallerEmail}`}>
-                                    Email
-                                </a>
-                            </div>
+                </div>
+                <div className="footer-column">
+                    <div className="footer-subrow">
+                        <div className="footer-title">Contact Us</div>
+                        {renderSocialLinks()}
+                        <div>
+                            <a href={`mailto:${lymarPallerEmail}`}>
+                                Email
+                            </a>
                         </div>
                     </div>
                 </div>
-            </footer>
-
-        </>
+            </div>
+        </footer>
     );
 };
 
