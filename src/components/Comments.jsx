90836a2888b45b2a58505df90c40652092818571
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-09 22:34:11 +0800

    refactor: delete comment function

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,46 +1,57 @@
 import React, { useState } from 'react';
-import '../styles/Comments.scss'
+import '../styles/Comments.scss';
 import Profile from '../assets/wanda.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function Comments() {
     const [comments, setComments] = useState([
         {
             id: 1,
-            name: 'BG color - Red',
-            text: 'i want one',
+            name: 'Riya Villamor',
+            text: 'That dog is adorable!',
         },
         {
             id: 2,
-            name: 'Riya Kahoot Queen',
+            name: 'Lymar Paller',
             text:
-                'So cute!asdfasdfasdfsadffadfas asdfasdfasdfsadffadfasasdfasdf sadfasdfasdfasdfasdf asdfasdfasdfsadffadfasasdfasdfasdfasdf asdfasdfasdfsadffadfasasdfasdfasdfasdf asdfasdfasdfasdfsadf asdfasdfasdfsadffadfasaasdfsdf',
+                'I want to adopt this dog! Can I?',
         },
         {
             id: 3,
-            name: 'Riya Kahoot Queen',
+            name: 'Klane Zurbano',
             text:
-                'So cute!asdfasdfasdfsadffadfas asdfasdfasdfsadffadfasasdfasdf sadfasdfasdfasdfasdf asdfasdfasdfsadffadfasasdfasdfasdfasdf asdfasdfasdfsadffadfasasdfasdfasdfasdf asdfasdfasdfasdfsadf asdfasdfasdfsadffadfasaasdfsdf',
+                'This dog reminds me of my furry friend!',
         },
     ]);
+
+    const handleRemoveComment = (id) => {
+        const updatedComments = comments.filter(comment => comment.id !== id);
+        setComments(updatedComments);
+    };
+
     return (
-        <>
-            <div className='list-comments'>
-                <div className='profile'>
-                    {comments.map((comment) => (
-                        <div key={comment.id} className='profile-image'>
-                            <img src={Profile} alt='Profile' />
-                            <div className='comment'>
-                                <h5>{comment.name}</h5>
-                                <div>
-                                    <p>{comment.text}</p>
-                                </div>
+        <div className='list-comments'>
+            <div className='profile'>
+                {comments.map((comment) => (
+                    <div key={comment.id} className='profile-image'>
+                        <img src={Profile} alt='Profile' />
+                        <div className='comment'>
+                            <h5>
+                                {comment.name}
+                                <span className='remove-comment' onClick={() => handleRemoveComment(comment.id)}>
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </span>
+                            </h5>
+                            <div>
+                                <p>{comment.text}</p>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default Comments
+export default Comments;
