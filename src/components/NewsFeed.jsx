016e4cb12c1a8675099f43ecd038981e1dd9abf6
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-08-10 22:56:09 +0800

    test: add post modal

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -7,70 +7,24 @@ import {faTimes} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function NewsFeed() {
-    const [post, setPost] = useState('');
-    const [feedItems, setFeedItems] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const newPost = {
-            id: Date.now(),
-            content: post,
-            author: 'Wanda Zurbano',
-            timestamp: new Date().toLocaleString(),
-            likes: 0,
-            comments: [],
-        };
-
-        setFeedItems([newPost, ...feedItems]);
-        setPost('');
-        setIsModalOpen(false); // Close the modal after submitting
-    };
-
-    const handleClickToPost = () => {
-        setIsModalOpen(true);
-    }
-
-    const handleCancelPost = () => {
-        setIsModalOpen(false);
-        setPost(''); 
-        // const backgroundSetClass = document.getElementsByClassName('background-overlay');
-        // for (const bgclass of backgroundSetClass) {
-        //     if(backgroundSetClass) {
-        //         bgclass.classList.add('background-overlay-new');
-        //         bgclass.classList.remove('background-overlay');
-        //     } else {
-        //         bgclass.classList.add('background-overlay-new');
-        //         bgclass.classList.remove('background-overlay');
-        //     }
-        // }
-    }
+    
+    // <FontAwesomeIcon icon={faTimes} className="logout-xmark" onClick={handleCancelPost}/>
 
     return (
         <div className="news-feed-container">
-            {isModalOpen && <div className="background-overlay" onClick={handleCancelPost}></div>}
-            <div className={`create-post ${isModalOpen ? 'new-create-post' : ''}`}>
+            <div className='create-post'>
                 <h4 className='create-post-header'>Create Post</h4>
-                <FontAwesomeIcon icon={faTimes} className="logout-xmark" onClick={handleCancelPost}/>
                 <div className="profile-pic">
                     <img src={Profile} alt="Profile" />
                 </div>
                 <div className='upload-photo'>
                     <input type="file" id="myFile" name="filename" className='upload-button' />
                 </div>
-                <form onSubmit={handleSubmit} className='form-container'>
-                    <textarea
-                        type="text"
-                        className="input-feed"
-                        placeholder="What's on your mind?"
-                        value={post}
-                        onChange={(e) => setPost(e.target.value)}
-                        onClick={handleClickToPost}
-                    />
+                <form className='form-container'>
+                    <textarea type="text" className="input-feed" placeholder="What's on your mind?"/>
                     <button type="submit">Post</button>
                 </form>
-                <button type="cancel" className="cancel-button" onClick={handleCancelPost}>Cancel</button>
+                <button type="cancel" className="cancel-button">Cancel</button>
             </div>
             <Feed />
         </div>
