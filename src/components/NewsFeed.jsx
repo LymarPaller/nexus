cdddcda2e409fdb5f409119bdfd787fc5577e9a2
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-10 23:40:45 +0800

    test: form to reactmodal in post polish

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,33 +1,77 @@
-// NewsFeed.js
 import React, { useState } from 'react';
-import "../styles/NewsFeed.scss";
-import Feed from "./Feed";
-import Profile from '../assets/wanda.jpg';
-import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import Modal from 'react-modal';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Profile from '../assets/wanda.jpg';
+import '../styles/NewsFeed.scss';
+import Feed from './Feed';
 
 function NewsFeed() {
-    
-    // <FontAwesomeIcon icon={faTimes} className="logout-xmark" onClick={handleCancelPost}/>
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [postText, setPostText] = useState('');
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    const handlePost = (event) => {
+        event.preventDefault();
+        console.log('Posted:', postText);
+
+         
+        setPostText('');
+        closeModal();
+    };
 
     return (
-        <div className="news-feed-container">
-            <div className='create-post'>
+        <>
+            <div className="news-feed-container">
+                <div className='create-post'>
+                    <h4 className='create-post-header'>Create Post</h4>
+                    <div className="profile-pic">
+                        <img src={Profile} alt="Profile" />
+                    </div>
+                    <textarea
+                        onClick={openModal}
+                        className="input-feed"
+                        placeholder="What's on your mind?"
+                        value={postText}
+                        onChange={(e) => setPostText(e.target.value)}
+                    />
+                    <button onClick={openModal} className="cancel-button">Cancel</button>
+                </div>
+                <Feed />
+            </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                className="new-create-post"
+                overlayClassName="modal-overlay"
+            >
                 <h4 className='create-post-header'>Create Post</h4>
+                <FontAwesomeIcon icon={faTimes} className="logout-xmark" onClick={closeModal} />
                 <div className="profile-pic">
                     <img src={Profile} alt="Profile" />
                 </div>
                 <div className='upload-photo'>
                     <input type="file" id="myFile" name="filename" className='upload-button' />
                 </div>
-                <form className='form-container'>
-                    <textarea type="text" className="input-feed" placeholder="What's on your mind?"/>
-                    <button type="submit">Post</button>
+                <form className='form-container' onSubmit={handlePost}>
+                    <textarea
+                        className="input-feed"
+                        placeholder="What's on your mind?"
+                        value={postText}
+                        onChange={(e) => setPostText(e.target.value)}
+                    />
+                    <button type="submit" className="post-button">Post</button>
                 </form>
-                <button type="cancel" className="cancel-button">Cancel</button>
-            </div>
-            <Feed />
-        </div>
+                <button onClick={closeModal} className="cancel-button">Cancel</button>
+            </Modal>
+        </>
     );
 }
 
