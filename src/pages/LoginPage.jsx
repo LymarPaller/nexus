4c62a732dd4bbee938f23c34ed1cd5e5003da8e8
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-16 23:03:31 +0800

    added: form validation and style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,78 +1,100 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import '../styles/LoginPage.scss';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from '../assets/nexus-logo-blue.svg';
 
-
 function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  });
 
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      showPassword: false,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log('Username:', values.username);
+      console.log('Password:', values.password);
+    },
+  });
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-
-        console.log('Username:', username);
-        console.log('Password:', password);
-    };
-
-    return (
-        <>
-            <div className='login-page-main-container'>
-
-                <div className='logo-container'>
-                    <img src={Logo} alt="Nexus-logo" />
-                </div>
-
-                <div className='card-container-login'>
-
-                    <div className='image-main-container'>
-                        <div className='image-container-login-front'>
-                        </div>
-                        <div className='image-container-login-back'>
-                        </div>
-                    </div>
-
-                    <div className='login-container'>
-                        <h1>NeXus</h1>
-                        <form action="" className='form-login' onSubmit={handleLogin}>
-                            <input
-                                type="text"
-                                placeholder='Username'
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
-                            <div className="password-input-container">
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder='Password'
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                                <button
-                                    type="button"
-                                    className="password-toggle-button"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="eye-icon" />
-                                </button>
-                            </div>
-                            <div className='button-container'>
-                                <button className='login-button' type='submit'>Login</button>
-                                <Link className='create-new-account' to="/register">Create New Account</Link>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div className='login-page-main-container'>
+        <div className='logo-container'>
+          <img src={Logo} alt='Nexus-logo' />
+        </div>
+        <div className='card-container-login'>
+          <div className='image-main-container'>
+            <div className='image-container-login-front'></div>
+            <div className='image-container-login-back'></div>
+          </div>
+          <div className='login-container'>
+            <h1>NeXus</h1>
+            <form className='form-login' onSubmit={formik.handleSubmit}>
+              <div className='input-container'>
+                <input
+                  type='text'
+                  placeholder='Username'
+                  {...formik.getFieldProps('username')}
+                />
+                {formik.touched.username && formik.errors.username ? (
+                  <div className='error-container'>
+                    <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                    {formik.errors.username}
+                  </div>
+                ) : null}
+              </div>
+              <div className='input-container'>
+                <input
+                  type={formik.values.showPassword ? 'text' : 'password'}
+                  placeholder='Password'
+                  {...formik.getFieldProps('password')}
+                />
+                <button
+                  type='button'
+                  className='password-toggle-button'
+                  onClick={() =>
+                    formik.setFieldValue(
+                      'showPassword',
+                      !formik.values.showPassword
+                    )
+                  }
+                >
+                  <FontAwesomeIcon
+                    icon={formik.values.showPassword ? faEye : faEyeSlash}
+                    className='eye-icon'
+                  />
+                </button>
+                {formik.touched.password && formik.errors.password ? (
+                  <div className='error-container'>
+                    <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+              <div className='button-container'>
+                <button className='login-button' type='submit'>
+                  Login
+                </button>
+                <Link className='create-new-account' to='/register'>
+                  Create New Account
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,86 +1,112 @@
-import { useState } from 'react';
-import '../styles/RegisterPage.scss';
+import React from 'react';
 import Logo from '../assets/nexus-logo-blue.svg';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import { useFormik } from 'formik';
-
+import * as Yup from 'yup';  
+import '../styles/RegisterPage.scss';
 
 function RegisterPage() {
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            name: '',
-            email: '',
-            password: '',
-        },
-        
-        onSubmit: (value) => {
-            console.log(value)
-        }
-    })
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
 
-    return (
-        <>
-            <div className='logo-container'>
-                <img src={Logo} alt="Nexus-logo" />
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,  
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <>
+      <div className='logo-container'>
+        <img src={Logo} alt='Nexus-logo' />
+      </div>
+
+      <div className='card-container-login'>
+        <div className='signup-login-container'>
+          <h1>Sign Up</h1>
+          <form action='' className='form-login form-signup' onSubmit={formik.handleSubmit}>
+            <input
+              id='username'
+              type='text'
+              placeholder='Username'
+              {...formik.getFieldProps('username')}
+            />
+            {formik.touched.username && formik.errors.username ? (
+              <div className='error-container'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                {formik.errors.username}
+              </div>
+            ) : null}
+
+            <input
+              id='name'
+              type='text'
+              placeholder='Name'
+              {...formik.getFieldProps('name')}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <div className='error-container'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                {formik.errors.name}
+              </div>
+            ) : null}
+
+            <input
+              id='email'
+              type='email'
+              placeholder='Email'
+              {...formik.getFieldProps('email')}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className='error-container'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                {formik.errors.email}
+              </div>
+            ) : null}
+
+            <input
+              id='password'
+              type='password'
+              placeholder='Password'
+              {...formik.getFieldProps('password')}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div className='error-container'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='error-icon' />
+                {formik.errors.password}
+              </div>
+            ) : null}
+
+            <div className='button-container button-register'>
+              <button className='button-register' type='submit'>
+                Register
+              </button>
+              <div className='back-button-container back-button-register'>
+                <Link to='/login' className='link-button'>
+                  <button type='submit' className='register-back-button'>
+                    <FontAwesomeIcon icon={faArrowLeft} className='back-icon' /> Back to Login
+                  </button>
+                </Link>
+              </div>
             </div>
-
-            <div className='card-container-login'>
-
-                <div className='signup-login-container'>
-                    <h1>Sign Up</h1>
-                    <form action="" className='form-login form-signup' onSubmit={formik.handleSubmit}>
-
-                        <input
-                            id='username'
-                            type="text"
-                            placeholder='Username'
-                            value={formik.values.username}
-                            onChange={formik.handleChange}
-                            required />
-
-                        <input
-                            id='name'
-                            type="text"
-                            placeholder='Name'
-                            value={formik.values.name}
-                            onChange={formik.handleChange}
-                            required />
-
-                        <input
-                            id='email'
-                            type="email"
-                            placeholder='Email'
-                            value={formik.values.email}
-                            onChange={formik.handleChange}
-                            required />
-
-                        <input
-                            id='password'
-                            type="password"
-                            placeholder='Password'
-                            value={formik.values.password}
-                            onChange={formik.handleChange}
-                            required />
-
-                        <div className='button-container button-register'>
-                            <button className='button-register' type='submit'>Register</button>
-                            <div className='back-button-container back-button-register'>
-                                <Link to="/login" className='link-button'>
-                                    <button type='submit' className='register-back-button'>
-                                        <FontAwesomeIcon icon={faArrowLeft} className="back-icon" /> Back to Login
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-        </>
-    );
-};
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default RegisterPage;
