9976f5c51bd813c31146ba52bb7e2bd0b6de2067
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-08-20 18:09:01 +0800

    feat: add post comment in comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -7,13 +7,22 @@ import { useModal } from '../app/useModal';
 import '../styles/modals/modal.scss';
 import '../styles/Comments.scss';
 import axios from 'axios';
+import { useFormik } from 'formik';
 
-function Comments({ feedItems, postId }) {
+function Comments({ feedItems, postId, closeModal }) {
 
     const { isOpen: showDeleteModal, openModal: openDeleteModal, closeModal: closeDeleteModal } = useModal();
     const [commentToDelete, setCommentToDelete] = useState(null);
     // const [comment, setComment] = useState('');
     const [comments, setComments] = useState([])
+
+    
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    
+    const formattedDate = `${year}-${month}-${day}`;
 
     const fetchFeed = async () => {
         try {
@@ -30,24 +39,36 @@ function Comments({ feedItems, postId }) {
     }, []);
     
 
-    const handleDeleteComment = () => {
-    if (commentToDelete !== null) {
-        const updatedComments = feedItems.filter((item, index) => index !== commentToDelete);
-        // you can update soon like this  setComments(updatedComments);
-        setCommentToDelete(null);
-        closeDeleteModal();
-    }
-    };
-  
-    const handleCommentSubmit = (event) => {
-        event.preventDefault();
-        // if (comment.trim()) {
-        // // Add logic to submit comment to the server or update state
-        // setComment('');
-        // }
+    const handleDeleteComment = async (commentId) => {
+        try {
+            const res = await axios.delete(`http://localhost:8000/api/v1/comments/${commentId}`);
+            
+            fetchFeed();
+            closeDeleteModal();
+        } catch (error) {
+            console.error('Error deleting comment:', error);
+        }
     };
 
-    console.log(comments)
+    
+    const formik = useFormik({
+        initialValues: {
+        postId: postId,
+        commentDescription: '',
+        dateCommented: formattedDate,
+        userId: '66',
+        },
+        onSubmit: async (values) => {
+        try {
+            const res = await axios.post('http://localhost:8000/api/v1/comments', values);
+            console.log('Post successful: ', res.data);
+            formik.resetForm();
+            fetchFeed();
+        } catch (error) {
+            console.error('Posting comment failed: ', error)
+        }
+        }
+    });
 
     return (
         <div className='list-comments'>
@@ -55,15 +76,15 @@ function Comments({ feedItems, postId }) {
             {comments.map((comment)=>(
                 <div className='profile' key={comment.id}>
                     <div className='profile-image'>
-                        <img src={comment.user.profilePhoto} alt='Profile' />
+                        <img src={comment.user.profile_photo} alt='Profile' />
                         <div className='comment'>
                             <h5>{comment.user.name}</h5>
                             <div>
                                 <p>{comment.commentDescription}</p>
                             </div>
                             <span className='remove-comment' onClick={() => {
-                                setCommentToDelete();
                                 openDeleteModal(comment.id); // Open the delete modal
+                                setCommentToDelete(comment.id)
                             }}>
                                 <FontAwesomeIcon icon={faTrash} className='trash-icon' />
                             </span>
@@ -73,45 +94,48 @@ function Comments({ feedItems, postId }) {
                 ))
             }
                 
-
-            <div className="modal-body">
-                <textarea
-                    type="text"
-                    placeholder="Write a comment..."
-                    // value={comment}
-                    // onChange={(e) => setComment(e.target.value)}
-                />
-            </div>
-            <div className="modal-footer">
-                <button onClick={handleCommentSubmit} className="add-comment-button">
-                    Add Comment
-                </button>
-            </div>
-
-            {/* Confirmation Modal */}
-            <Modal
-                isOpen={showDeleteModal}
-                onRequestClose={closeDeleteModal}
-                className="modal-delete-comment"
-                overlayClassName="modal-overlay"
-                ariaHideApp={false}
-            >
-                <div className="logout-header">
-                    <h5>Delete Confirmation</h5>
-                    <button
-                        className="modal-close-button"
-                        onClick={closeDeleteModal} // Close the modal on button click
-                    >
+            <form onSubmit={formik.handleSubmit}>
+                <div className="modal-body">
+                    <textarea
+                        id='commentDescription'
+                        type="text"
+                        placeholder="Write a comment..."
+                        {...formik.getFieldProps('commentDescription')}
+                    />
+                </div>
+                <div className="modal-footer">
+                    <button className="add-comment-button" type="submit">
+                        Add Comment
                     </button>
                 </div>
-                <div className="modal-content">
-                    <p>Are you sure you want to delete this comment?</p>
-                    <div className="modal-buttons">
-                        <button className="add-btn" onClick={closeDeleteModal}>Cancel</button>
-                        <button className="remove-btn" onClick={handleDeleteComment}>Delete</button>
+            </form>
+
+            {/* Confirmation Modal */}
+            <div>
+                <Modal
+                    isOpen={showDeleteModal}
+                    onRequestClose={closeDeleteModal}
+                    className="modal-delete-comment"
+                    overlayClassName="modal-overlay"
+                    ariaHideApp={false}
+                >
+                    <div className="logout-header">
+                        <h5>Delete Confirmation</h5>
+                        <button
+                            className="modal-close-button"
+                            onClick={closeDeleteModal} // Close the modal on button click
+                        >
+                        </button>
                     </div>
-                </div>
-            </Modal>
+                    <div className="modal-content">
+                        <p>Are you sure you want to delete this comment?</p>
+                        <div className="modal-buttons">
+                            <button className="add-btn" onClick={closeDeleteModal}>Cancel</button>
+                            <button className="remove-btn" onClick={() => handleDeleteComment(commentToDelete)}>Delete</button>
+                        </div>
+                    </div>
+                </Modal>
+            </div>
         </div>
     );
 }
