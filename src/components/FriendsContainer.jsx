9198c70b232b723e2b93b9f69927671d2eeaf819
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-08-21 17:15:52 +0800

    feat: shows friends

--- src/components/FriendsContainer.jsx
+++ src/components/FriendsContainer.jsx
@@ -1,103 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Profile from '../assets/wanda.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH, faUserTimes, faUserMinus } from "@fortawesome/free-solid-svg-icons";
 import Modal from 'react-modal';
 import { useModal } from '../app/useModal';
+import axios from "axios";
 
-function FriendsContainer() {
+function FriendsContainer({ followerName, profilePhoto}) {
     const optionsModal = useModal();
     const unfollowModal = useModal();
     const unfriendModal = useModal();
 
     const handleUnfollow = () => {
-        // Implement unfollow logic here
         unfollowModal.closeModal();
         
     };
 
     const handleUnfriend = () => {
-        // Implement unfriend logic here
         unfriendModal.closeModal();
     };
 
     return (
-        <div className="friend-container">
-                <div className="online-friends">
-                    <div className="aside-right-image">
-                        <img src={Profile} alt="Profile" />
+            <div className="online-friends">
+                <div className="aside-right-image">
+                    <img src={profilePhoto} alt="Profile" />
+                </div>
+                <div className="friend-details">
+                    <span className="friend-name">{followerName}
+                        <FontAwesomeIcon
+                            icon={faEllipsisH}
+                            className="ellipsis-icon"
+                            onClick={optionsModal.openModal}
+                        />
+                    </span>
+                </div>
+        
+                <Modal
+                    isOpen={optionsModal.isOpen}
+                    onRequestClose={optionsModal.closeModal}
+                    className="modal-options"
+                    overlayClassName="modal-overlay"
+                    ariaHideApp={false}
+                >
+                    {/* Options Modal content */}
+                    <div className="modal-content friend-option">
+                        <button className="add-btn" onClick={unfollowModal.openModal}>
+                            <FontAwesomeIcon icon={faUserTimes}  />
+                            Unfollow
+                        </button>
+                        <button className="remove-btn" onClick={unfriendModal.openModal}>
+                            <FontAwesomeIcon icon={faUserMinus} />
+                            Unfriend
+                        </button>
                     </div>
-                    <div className="friend-details">
-                        <span className="friend-name">friend 1
-                            <FontAwesomeIcon
-                                icon={faEllipsisH}
-                                className="ellipsis-icon"
-                                onClick={optionsModal.openModal}
-                            />
-                        </span>
-                    </div>
-            
-                    <Modal
-                        isOpen={optionsModal.isOpen}
-                        onRequestClose={optionsModal.closeModal}
-                        className="modal-options"
-                        overlayClassName="modal-overlay"
-                        ariaHideApp={false}
-                    >
-                        {/* Options Modal content */}
-                        <div className="modal-content friend-option">
-                            <button className="add-btn" onClick={unfollowModal.openModal}>
-                                <FontAwesomeIcon icon={faUserTimes}  />
+                </Modal>
+
+                <Modal
+                    isOpen={unfollowModal.isOpen}
+                    onRequestClose={unfollowModal.closeModal}
+                    className="modal-delete-comment"
+                    overlayClassName="modal-overlay"
+                    ariaHideApp={false}
+                >
+                    {/* Unfollow Modal content */}
+                    <div className="modal-content">
+                        <p>Are you sure you want to unfollow this user?</p>
+                        <div className="modal-buttons">
+                            <button className="add-btn" onClick={unfollowModal.closeModal}>Cancel</button>
+                            <button className="remove-btn" onClick={handleUnfollow}>
+                                <FontAwesomeIcon icon={faUserTimes} className="icon" />
                                 Unfollow
                             </button>
-                            <button className="remove-btn" onClick={unfriendModal.openModal}>
-                                <FontAwesomeIcon icon={faUserMinus} />
+                        </div>
+                    </div>
+                </Modal>
+
+                <Modal
+                    isOpen={unfriendModal.isOpen}
+                    onRequestClose={unfriendModal.closeModal}
+                    className="modal-delete-comment"
+                    overlayClassName="modal-overlay"
+                    ariaHideApp={false}
+                >
+                    {/* Unfriend Modal content */}
+                    <div className="modal-content">
+                        <p>Are you sure you want to unfriend this user?</p>
+                        <div className="modal-buttons">
+                            <button className="add-btn" onClick={unfriendModal.closeModal}>Cancel</button>
+                            <button className="remove-btn" onClick={handleUnfriend}>
+                                <FontAwesomeIcon icon={faUserMinus} className="icon" />
                                 Unfriend
                             </button>
                         </div>
-                    </Modal>
-
-                    <Modal
-                        isOpen={unfollowModal.isOpen}
-                        onRequestClose={unfollowModal.closeModal}
-                        className="modal-delete-comment"
-                        overlayClassName="modal-overlay"
-                        ariaHideApp={false}
-                    >
-                        {/* Unfollow Modal content */}
-                        <div className="modal-content">
-                            <p>Are you sure you want to unfollow this user?</p>
-                            <div className="modal-buttons">
-                                <button className="add-btn" onClick={unfollowModal.closeModal}>Cancel</button>
-                                <button className="remove-btn" onClick={handleUnfollow}>
-                                    <FontAwesomeIcon icon={faUserTimes} className="icon" />
-                                    Unfollow
-                                </button>
-                            </div>
-                        </div>
-                    </Modal>
-
-                    <Modal
-                        isOpen={unfriendModal.isOpen}
-                        onRequestClose={unfriendModal.closeModal}
-                        className="modal-delete-comment"
-                        overlayClassName="modal-overlay"
-                        ariaHideApp={false}
-                    >
-                        {/* Unfriend Modal content */}
-                        <div className="modal-content">
-                            <p>Are you sure you want to unfriend this user?</p>
-                            <div className="modal-buttons">
-                                <button className="add-btn" onClick={unfriendModal.closeModal}>Cancel</button>
-                                <button className="remove-btn" onClick={handleUnfriend}>
-                                    <FontAwesomeIcon icon={faUserMinus} className="icon" />
-                                    Unfriend
-                                </button>
-                            </div>
-                        </div>
-                    </Modal>
-                </div>
-        </div>
+                    </div>
+                </Modal>
+            </div>
     );
 }
 
--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,23 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AsideLeft from "../components/AsideLeft";
 import AsideRight from "../components/AsideRight";
 import FriendsContainer from "../components/FriendsContainer"
 import "../styles/FriendsPage.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUser } from "../store/currentUserReducer";
+import axios from "axios";
 
 function FriendsPage() {
+  const dispatch = useDispatch()
+  const [currentUser, setCurrentUser] = useState([])
+  const [followers, setfollowers] = useState([])
+
+  //SET-USER-TEMPORARY
+  const user = {
+    id: 66,
+    username: 'wandaring',
+    name: 'Wanda Zurbano',
+    profilePhoto: 'https://images.pexels.com/photos/18025212/pexels-photo-18025212/free-photo-of-wanda-the-wonderdog.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  }
+
+  const fetchFollower = async () => {
+    try {
+        const res = await axios(`http://localhost:8000/api/v1/follower?followUserId=${currentUser}`)
+        setfollowers(res.data.data)
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+    }
+  }
   
  
   useEffect(() => {
-    document.title = 'Friends';  
+    document.title = 'Friends';
+    setCurrentUser(user);
+    fetchFollower();
   }, []);
 
+  useEffect(() => {
+    console.log(followers);
+  }, [followers]); 
+  
   return (
     <div className="friends-main-container">
       <AsideLeft />
       <div className="friends-page-container">
         <div className="friend-container">
           <h1>Friends</h1>
-          <FriendsContainer />
+          <div className="friend-container">
+            {followers.map(follower => (
+              <FriendsContainer 
+                key={follower.id} 
+                followerUserId={follower.followerUserId}
+                followerName={follower.followerUser.name}
+                profilePhoto={follower.followerUser.profilePhoto}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
