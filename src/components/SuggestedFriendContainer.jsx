2b559a26e7213deb8cd63692c7c0dd4568137251
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-23 04:19:15 +0800

    clean: remove unnecessary imports from suggested friends

--- src/components/SuggestedFriendContainer.jsx
+++ src/components/SuggestedFriendContainer.jsx
@@ -1,33 +1,29 @@
-
-import Profile from '../assets/wanda.jpg';
 import React, { useState } from 'react';
 
-function SuggestedFriendContainer (props) {
-const {profilePhoto} = props
-    const [suggestedFriends, setSuggestedFriends] = useState([
-        { name: 'Faith Mariano' },
-        { name: 'Saylor Twift' }
-    ]);
+function SuggestedFriend({ profilePhoto }) {
+  const [suggestedFriends, setSuggestedFriends] = useState([
+    { name: 'Faith Mariano' },
+    { name: 'Saylor Twift' }
+  ]);
 
-    return(
-        <>
-        {/* Add unique Id soon index part */}
-        {suggestedFriends.map((friend, index) => (
-            <div className="friend-suggestion" key={index}>
-                <div className="aside-right-image">
-                    <img src={profilePhoto} alt="Profile" />
-                </div>
-                <div className="suggest-name-container">
-                    <span>{friend.name}</span>
-                    <div className="button-add-remove">
-                        <button className="add-btn">Add</button>
-                        <button className="remove-btn">Remove</button>
-                    </div>
-                </div>
+  return (
+    <div className="suggested-friend-container">
+      {suggestedFriends.map((friend, index) => (
+        <div className="friend-suggestion" key={index}>
+          <div className="friend-suggestion-image">
+            <img src={profilePhoto} alt="Profile" />
+          </div>
+          <div className="friend-suggestion-info">
+            <span>{friend.name}</span>
+            <div className="friend-suggestion-buttons">
+              <button className="add-button">Add</button>
+              <button className="remove-button">Remove</button>
             </div>
-        ))}
-        </>
-    ) 
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default SuggestedFriendContainer
+export default SuggestedFriend;
